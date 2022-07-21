9588f6f86c6430ada2702e64f2de1629cb408566
Bucky Lee <2209203187@qq.com>
2022-07-21 15:33:45 +0800

    create web proj

---
+++ osap_web/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="logo" style="float: left">
+    <img alt="IMED logo" src="./assets/logo.png" width="128" height="39">
+  </div>
+
+  <br/>
+
+  <MainMenu></MainMenu>
+</template>
+
+<script>
+import MainMenu from './components/Main-Menu'
+
+export default {
+  name: 'App',
+  components: {
+    MainMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  position: relative;
+  left: 0%;
+  top: 0%;
+}
+</style>
---
+++ osap_web/src/components/Main-Menu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="Menu">
+    <ul>
+      <li><a href="#home">主页</a></li>
+      <li><a href="#function">功能</a></li>
+      <li><a href="#about">关于</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainMenu"
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #3039a1;
+  position: relative;
+  left: 0%;
+
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: grey;
+}
+
+li a.active{
+  background-color: #fc6509;
+  color: white;
+}
+
+</style>
