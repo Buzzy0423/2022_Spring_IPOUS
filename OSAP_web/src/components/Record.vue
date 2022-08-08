a5183646f81047ec576176640d0b64e503a7a357
zhangxiaotian2022 <99779749+zhangxiaotian2022@users.noreply.github.com>
2022-08-08 17:26:26 +0800

    Update Record.vue

--- OSAP_web/src/components/Record.vue
+++ OSAP_web/src/components/Record.vue
@@ -1,64 +1,67 @@
 <template>
-  <div class='model_record'>
-    <div style=" padding: 15px">
-      <!--  题目-->
-      <div style="margin: 15px">
-        模型选择
-      </div>
-      <div style="margin: 15px">
-        <el-select v-model="value" clearable placeholder="Select">
-          <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-          />
-        </el-select>
-      </div>
-      <!--  题目-->
-      <div style="margin: 15px">
-        处理记录
-      </div>
-      <!--  功能区-->
-      <el-card
-          id="record"
-          class="record"
-          style="border-radius: 8px" shadow="never"
-      >
-      <div style="margin: 15px">
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column fixed prop="date" label="日期" width="150"/>
-          <el-table-column prop="model" label="模型" width="120"/>
-          <el-table-column fixed="right" label="操作" width="300">
-            <template #default>
-              <el-button link type="primary" size="small" @click="checkRecord"
-              >查看记录
-              </el-button>
-              <el-button link type="danger" size="small">下载</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-      </div>
-      <div>
-        <el-dialog v-model="dialogVisible" title="记录">
-          记录
-          <el-button link type="primary" size="small" @click="certainInR">
 
-          </el-button>
-        </el-dialog>
-      </div>
-      </el-card>
+  <div style=" padding: 15px">
+    <!--  题目-->
+    <div style="margin: 15px">
+      模型选择
     </div>
+    <div style="margin: 15px">
+      <el-select v-model="value" clearable placeholder="Select">
+        <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+        />
+      </el-select>
+    </div>
+    <!--  题目-->
+    <div style="margin: 15px">
+      处理记录
+    </div>
+    <!--  功能区-->
+    <div style="margin: 15px">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column fixed prop="date" label="日期" width="150" />
+        <el-table-column prop="model" label="模型" width="120" />
+        <el-table-column prop="address" label="图片" sortable width="180" align="center" header-align="center">
+          <template #default ="scope">
+            <el-image style="width: 100%; height: 100px" :src="scope.row.address" :preview-src-list="[scope.row.address]" :key="scope.row.id" preview-teleported="true" >
+              <div slot="error" class="image-slot">
+                <i class="el-icon-picture-outline"></i>
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+
+
+        <el-table-column fixed="right" label="操作" width="300">
+          <template #default>
+            <el-button link type="danger" size="small">下载</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div>
+      <el-dialog v-model = "dialogVisible" title="记录">
+        记录
+        <el-button link type="primary" size="small" @click = "certainInR" >
+
+        </el-button>
+      </el-dialog>
+    </div>
+
   </div>
+
 </template>
 
 <script>
 export default {
   name: "Record",
-  data() {
+  data(){
     return {
       dialogVisible: false,
-      options: [
+      options:[
         {
           value: 'Option1',
           label: 'Option1',
@@ -80,31 +83,35 @@ export default {
           label: 'Option5',
         },
       ],
-      tableData: [
+      tableData : [
         {
           date: '2022-7-21',
           model: '不知道',
+          address:require('D:\\springboot-vue\\springboot-vue-demo\\vue\\src\\assets\\a.png'),
         },
         {
           date: '2022-7-21',
           model: '不知道',
+          address:require('D:\\springboot-vue\\springboot-vue-demo\\vue\\src\\assets\\a.png'),
         },
         {
           date: '2022-7-21',
           model: '不知道',
+          address:require('D:\\springboot-vue\\springboot-vue-demo\\vue\\src\\assets\\a.png'),
         },
         {
           date: '2022-7-21',
           model: '不知道',
+          address:require('D:\\springboot-vue\\springboot-vue-demo\\vue\\src\\assets\\a.png'),
         },
       ]
     }
   },
-  methods: {
-    checkRecord() {
+  methods : {
+    checkRecord(){
       this.dialogVisible = true
     },
-    certainInR() {
+    certainInR(){
       this.dialogVisible = false
     }
   }
@@ -113,4 +120,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
