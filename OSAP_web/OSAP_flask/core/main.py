2b498f0260ee4d894313797ebeb985e3d644c308
GaoYunshu1 <92198325+GaoYunshu1@users.noreply.github.com>
2022-11-29 20:40:47 +0800

    Add files via upload

--- OSAP_web/OSAP_flask/core/main.py
+++ OSAP_web/OSAP_flask/core/main.py
@@ -1,7 +1,23 @@
-from core import predict, process
+from core.predict import predict
+from core.process import preprocess
+from PIL import Image
+import cv2
+
 
 def process(file_path, model_name):
+    """
+
+    :param file_path: the path of cataract fundus image
+    :param model_name: the model you want to use
+    """
     # TODO: fill this
+    image = Image.open(file_path)
+    image_name = file_path.split('/')[-1].split('.')[0]
+    processed_image = preprocess(image)
+    restored_image = predict(processed_image, model_name)  # need returning a numpy array
+    restored_image = Image.fromarray(restored_image)
+    save_path = str(f'./data/{image_name}.png') 
+    restored_image.save(save_path)
     return 'Success'
 
 
